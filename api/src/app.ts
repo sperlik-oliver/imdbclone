6796e3d53619c0525dcff0cc9e4ecf8b16149cf4
sperlik-oliver <osp@whiteawaygroup.com>
2023-05-20 11:50:13 +0200

    add health and default route

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import dotenv from 'dotenv'
 import tokenMiddleware from './middleware/token.middleware';
 import { userRouter } from './routes/user/user.route';
+import { HTTP_STATUS_CODES } from '../../shared/types';
 
 dotenv.config()
 
@@ -11,9 +12,13 @@ const app = express()
 
 app.use(express.json())
 
+app.use('/health', (_, res) => res.status(HTTP_STATUS_CODES.OK).json("Service is healthy"))
+
 app.use('/', (req, res, next) => tokenMiddleware(req, res, next))
 
 app.use('/user', userRouter)
+    
+app.use('/', (_, res) => res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ data: null, errors: "Route not found" }))
 
 app.listen(PORT, () => {
     console.log(`Application is ready on port ${PORT}`)
--- shared/types.ts
+++ shared/types.ts
@@ -2,7 +2,8 @@ export const HTTP_STATUS_CODES = Object.freeze({
     OK: 200,
     BAD_REQUEST: 400,
     UNAUTHORIZED: 401,
-    INTERNAL_SERVER_ERROR: 500
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_FOUND: 404
 })
 
 export const ERRORS = Object.freeze({
