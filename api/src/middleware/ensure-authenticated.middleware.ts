77d13ac39eaebaae00c812c007dc1483a574576d
sperlik-oliver <osp@whiteawaygroup.com>
2023-05-25 17:45:43 +0200

    add logs

--- api/src/middleware/ensure-authenticated.middleware.ts
+++ api/src/middleware/ensure-authenticated.middleware.ts
@@ -4,11 +4,11 @@ import client from "../repository/client/client";
 import { HTTP_STATUS_CODES } from "../types";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-    const auth = req.header('ensure-auth')
-    if (!auth) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: 'Authentication not successful' })
-    const [token, email] = auth.split(' ')
-    if (!email || !token) if (!auth) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: 'Authentication not successful' })
     await safeQuery(async () => {
+        const auth = req.header('ensure-auth')
+        if (!auth) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: 'Authentication not successful' })
+        const [token, email] = auth.split(' ')
+        if (!email || !token) if (!auth) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: 'Authentication not successful' })
         const user = await client.user.findFirst({
             where: {
                 email,
--- api/src/middleware/token.middleware.ts
+++ api/src/middleware/token.middleware.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { ERRORS, HTTP_STATUS_CODES } from '../types'
 import dotenv from 'dotenv'
+import safeQuery from "../routes/safe-query/safe-query";
 
 dotenv.config()
 
 const AUTH_API_KEY = process.env.AUTH_API_KEY
 
-export default (req: Request, res: Response, next: NextFunction) => {
-    if (!AUTH_API_KEY) return res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({ data: "Missing API Key", error: ERRORS.INTERNAL_SERVER_ERROR })
-    const token = req.header('Authorization')
-    if (!token || token !== AUTH_API_KEY) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: ERRORS.INVALID_TOKEN })
-    next()
+export default async (req: Request, res: Response, next: NextFunction) => {
+    await safeQuery (async () => {
+        if (!AUTH_API_KEY) return res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({ data: "Missing API Key", error: ERRORS.INTERNAL_SERVER_ERROR })
+        const token = req.header('Authorization')
+        if (!token || token !== AUTH_API_KEY) return res.status(HTTP_STATUS_CODES.UNAUTHORIZED).json({ data: null, error: ERRORS.INVALID_TOKEN })
+        next()
+    }, res, 'token-middleware')
 }
